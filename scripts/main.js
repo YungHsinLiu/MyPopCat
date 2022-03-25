31352afc77a3d500f02d6d3e5aff155e45ae6c27
YungHsinLiu <102026525+YungHsinLiu@users.noreply.github.com>
2022-03-25 18:16:45 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,20 @@
 var isPopping = false;
 var timeoutId;
+var popCount = 0;
+
+var Count = document.querySelector('h1');
+
+
 function changeSrc()
 {
     clearTimeout(timeoutId);
-        document.getElementById("myPopCat").src="images/popcat2.jpeg" ;
-        timeoutId = setTimeout(function() {
-            document.getElementById("myPopCat").src="images/popcat1.jpeg" ;
-        }, 100); 
-    
+    popCount++;
+    Count.textContent = popCount.toString();
+    document.getElementById("myPopCat").src="images/popcat2.jpeg" ;
+    timeoutId = setTimeout(function() {
+        document.getElementById("myPopCat").src="images/popcat1.jpeg" ;
+    }, 100); 
+
         // clearTimeout(timer);
 }
 

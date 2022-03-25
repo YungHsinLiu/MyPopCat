d2d57c49edcfd2dd000b3e6109bf364b9addd9cb
YungHsinLiu <102026525+YungHsinLiu@users.noreply.github.com>
2022-03-25 18:03:58 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,11 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = "Hello World!";
 var isPopping = false;
 var timeoutId;
 function changeSrc()
 {
     clearTimeout(timeoutId);
-        document.getElementById("myImage").src="images/popcat2.jpeg" ;
+        document.getElementById("myPopCat").src="images/popcat2.jpeg" ;
         timeoutId = setTimeout(function() {
-            document.getElementById("myImage").src="images/popcat1.jpeg" ;
+            document.getElementById("myPopCat").src="images/popcat1.jpeg" ;
         }, 100); 
     
         // clearTimeout(timer);

45719e482fca0bf8fe9f419be6b392f4c6037f1d
YungHsinLiu <102026525+YungHsinLiu@users.noreply.github.com>
2022-03-28 10:28:20 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ function changeSrc()
     document.getElementById("myPopCat").src="images/popcat2.jpeg" ;
     timeoutId = setTimeout(function() {
         document.getElementById("myPopCat").src="images/popcat1.jpeg" ;
-    }, 100); 
+    }, 50); 
 
         // clearTimeout(timer);
 }
